b2acf71768bb01b6343210bbe6574f493f9d08ab
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-07-22 09:14:29 -0300

    listagem

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ let lembretes = {
 
 let arrayLembretes = []
 
-
-
-
+listar()
+function listar() {
+    if(lembretes.length === 0) {
+        console.log('nenhum lembrete cadastrado!')
+        // colocar aqui código para voltar para o menu, precionando enter
+    }
+    else {
+        console.log('Lista de lembretes: \n')
+        lembretes.forEach((lista, index) => {
+            console.log(`${index + 1}. Lembrete: ${lista.nome} \ndata para ser feita: ${lista.data} \nstatus: ${lista.stats}`)
+        })
+    }
+    console.log('precione enter para voltar')
+    // colocar aqui código para voltar para o menu, precionando enter
+}
